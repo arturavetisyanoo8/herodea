b295c3ff63b399329db4c853cf79bbcca7889e08
Artur <75808333+arturavetisyanoo8@users.noreply.github.com>
2023-07-03 17:39:26 +0400

    Add files via upload

---
+++ heroidea/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+
+	<title>Heroidea</title>
+</head>
+<body>
+	<div id="all">
+		<div id="header">
+			HEROIDEA
+		</div>
+		<div id="body"><br>
+			welcome to <br><a href="">HEROIDEA</a>
+		</div>
+	</div>
+</body>
+</html>
